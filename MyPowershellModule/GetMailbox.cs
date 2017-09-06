f83eb5e917ef0c3ce93df04f74d673ec708bcdf1
Derek Anderson <deanders@microsoft.com>
2017-09-06 07:40:58 +0100

    Add project files.

---
+++ MyPowershellModule/GetMailbox.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace MyPowershellModule
+{
+    [Cmdlet(VerbsCommon.Get, "MyMailbox")]
+    class GetMyMailbox : Cmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "Provide mailbox identity")]
+        public string Identity;
+
+        [Parameter(Mandatory = true, Position = 1, HelpMessage = "Provide organization")]
+        public string Organization;
+
+        protected override void BeginProcessing() //Used for initializing resources
+        {
+            ValidateParameters(); //We want to validate our parameters are set
+        }
+
+        protected override void ProcessRecord()
+        {
+            string firstGuid = string.Empty;
+
+            // We use the current runspace to cast our calls into.
+            // This grants us access to the current runspace, instead of instantiating a new one.
+            // More information on the PowerShell.Create method can be found here: https://msdn.microsoft.com/en-us/library/system.management.automation.powershell.create(v=vs.85).aspx
+            // More information on the RunspaceMode.CurrentRunspace enumeration value can be found here: https://msdn.microsoft.com/en-us/library/system.management.automation.runspacemode(v=vs.85).aspx
+            PowerShell myinitialPowerShell = PowerShell.Create(RunspaceMode.CurrentRunspace);
+            myinitialPowerShell.AddCommand("Get-Mailbox");
+            myinitialPowerShell.AddParameter("Identity", this.Identity);
+            myinitialPowerShell.AddParameter("Organization", this.Organization);
+
+            Collection<PSObject> resultsCollection = myinitialPowerShell.Invoke();
+            this.WriteObject(resultsCollection);
+        }
+
+        protected override void EndProcessing() //Executes after Begin Processing
+        {
+
+        }
+
+        protected override void StopProcessing() //Called when your cmdlet execution is interrupted. Use to clean up resources
+        {
+        }
+
+        /// <summary>
+        /// Validates Cmdlet parameters prior to processing the record
+        /// </summary>
+        private void ValidateParameters()
+        {
+            if (String.IsNullOrEmpty(Identity))
+                ThrowParameterError("Identity");
+
+            if (String.IsNullOrEmpty(Organization))
+                ThrowParameterError("Organization");
+        }
+        private void ThrowParameterError(string parameterName)
+        {
+            ThrowTerminatingError(
+                new ErrorRecord(
+                    new ArgumentException(String.Format(
+                        "Must pecifify '{0}'", parameterName)),
+                    Guid.NewGuid().ToString(),
+                    ErrorCategory.InvalidArgument,
+                    null));
+        }
+
+    }
+}
---
+++ MyPowershellModule/GetMailboxStatus.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation.Host;
+using System.Collections.ObjectModel;
+
+namespace MyPowershellModule
+{
+    [Cmdlet(VerbsCommon.Get, "MailboxStatus")]
+    public class GetMailboxStatus : Cmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, HelpMessage = "Provide a users MailboxGuid value")]
+        public string MailboxGuid;
+
+        [Parameter(Mandatory = true, Position = 1, HelpMessage = "Provide a database")]
+        public string Database;
+
+        protected override void BeginProcessing() //Used for initializing resources
+        {
+            try
+            {
+
+                ValidateParameters(); //We want to validate our parameters are set
+
+                PowerShell importScripts = PowerShell.Create(RunspaceMode.CurrentRunspace);
+
+                //Import the the PS1 allowing us to expose the Get-StoreQuery cmdlet
+                string script = @".\ManagedStoreDiagnosticFunctions.ps1";
+                importScripts.AddScript(script);
+                var results = importScripts.Invoke();
+                WriteObject(results);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        protected override void ProcessRecord()
+        {
+            PowerShell myfirstPowerShell = PowerShell.Create(RunspaceMode.CurrentRunspace);
+            string query = String.Format("select * from [{0}].Mailbox where MailboxGuid = '{1}'", this.Database, this.MailboxGuid);
+
+            // We add the command we want to run.
+            // More information on the PowerShell.AddCommand method can be found here: https://msdn.microsoft.com/en-us/library/dd182430(v=vs.85).aspx
+            myfirstPowerShell.AddCommand("Get-StoreQuery");
+
+            // We add the parameter that we want to specify.
+            // More information on the PowerShell.AddParameter method can be found here: https://msdn.microsoft.com/en-us/library/dd182434(v=vs.85).aspx
+
+            myfirstPowerShell.AddParameter("Database", this.Database).AddArgument(query);
+
+            // We add the command we want to run.
+            // More information on the PowerShell.AddCommand method can be found here: https://msdn.microsoft.com/en-us/library/dd182430(v=vs.85).aspx
+            myfirstPowerShell.AddCommand("Select-Object");
+
+            // We select the specific property we want to return.
+            // More information on the PowerShell.AddParameter method can be found here: https://msdn.microsoft.com/en-us/library/dd182434(v=vs.85).aspx
+            myfirstPowerShell.AddParameter("Property", new string[] { "MailboxGuid","MailboxNumber","DisplayName","DeletedOn","Status" });
+
+            Collection<PSObject> resultsCollection = myfirstPowerShell.Invoke();
+            PSObject myFirstPsObject = resultsCollection.FirstOrDefault();
+
+            if (myFirstPsObject != null)
+            {
+                //Write 
+                WriteObject(myFirstPsObject);
+            }
+        }
+
+        protected override void EndProcessing() //Executes after Begin Processing
+        {
+
+        }
+
+        protected override void StopProcessing() //Called when your cmdlet execution is interrupted. Use to clean up resources
+        {
+        }
+
+        /// <summary>
+        /// Validates Cmdlet parameters prior to processing the record
+        /// </summary>
+        private void ValidateParameters()
+        {
+            if (String.IsNullOrEmpty(MailboxGuid))
+                ThrowParameterError("MailboxGuid");
+
+            if (String.IsNullOrEmpty(Database))
+                ThrowParameterError("Database");
+        }
+        private void ThrowParameterError(string parameterName)
+        {
+            ThrowTerminatingError(
+                new ErrorRecord(
+                    new ArgumentException(String.Format(
+                        "Must pecifify '{0}'", parameterName)),
+                    Guid.NewGuid().ToString(),
+                    ErrorCategory.InvalidArgument,
+                    null));
+        }
+    }
+}
